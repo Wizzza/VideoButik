22100fefdeca03b1d0fcdef62d583c08cee09b00
Wizzza <jonas_eriksson_se@hotmail.com>
2018-02-01 22:52:17 +0100

    CustomerController added along with it's model, context. Setup index and create page for Movie and customer.

--- VideoButik2/Models/MovieModel.cs
+++ VideoButik2/Models/MovieModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace VideoButik2.Models
 {
     public class MovieModel
     {
+        [Key]
+        public int ID { get; set; }
         public string Name { get; set; }
         public int Length { get; set; }
         public string Genre { get; set; }
